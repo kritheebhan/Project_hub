1f69b45b47521ae6d5a8673a0547b2883323b8c7
kritheebhan <kritheebhan@gmail.com>
2023-11-02 18:31:44 +0530

    atles connected

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://127.0.0.1:27017/Project_hub")
+mongoose.connect("mongodb+srv://jai:2000@projecthub.qctvxrx.mongodb.net/projecthub")
 
 app.post("/login",(req, res) =>{
     const{email, password} = req.body;
